257eee5d35ffb3d93bce521ced4951174dd823a4
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-09 17:51:37 -0600

    Create 06_PiramideClass.java

---
+++ 06_PiramideClass.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        int contador = 1;
+        int contadorinicial = 1;
+        int contadorfinal = 2;
+
+        Scanner Teclado = new Scanner (System.in);
+        System.out.println("Escribe un numero entero positivo");
+        int numero = Teclado.nextInt();
+
+        while(contador <= numero){
+            while(contadorinicial < contadorfinal){
+                System.out.print("*");
+                contadorinicial = contadorinicial + 1;
+            }
+            contadorinicial = 1;
+            contadorfinal = contadorfinal + 1;
+            System.out.println();
+            contador = contador + 1;
+        }
+    }
+}
