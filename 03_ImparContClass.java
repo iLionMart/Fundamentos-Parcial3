748655ccc8133fac2aa74855684c5323c6d9e48b
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-09 17:44:57 -0600

    Create 03_ImparContClass.java

---
+++ 03_ImparContClass.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+
+        int contador = 1;
+
+        Scanner Teclado = new Scanner (System.in);
+        System.out.println("Escribe un numero entero positivo");
+        int numero = Teclado.nextInt();
+
+        while(contador <= numero){
+            if (contador % 2 == 0){
+                contador = contador + 1;
+            }else{
+                System.out.print(contador + " , ");
+                contador = contador + 1;
+            }
+        }
+    }
+}
