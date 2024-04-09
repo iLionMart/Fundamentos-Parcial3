ddc951a64114c183fbf7b851e4723364ba4e6c74
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-09 17:49:57 -0600

    Create 05_InversionClass.java

---
+++ 05_InversionClass.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        int contador = 1;
+        double resultado = 0;
+
+        Scanner Teclado = new Scanner (System.in);
+        System.out.println("Escribe tu inversion a realizar");
+        double inversion = Teclado.nextDouble();
+
+        Scanner Teclado2 = new Scanner (System.in);
+        System.out.println("Escribe la duracion de tu inversion a realizar");
+        int duracion = Teclado2.nextInt();
+
+        while(contador <= duracion){
+            resultado = resultado + inversion + (inversion * 0.11);
+            System.out.println("El " + contador + " año tu inversion es de " + resultado);
+            contador = contador + 1;
+        }
+    }
+}
