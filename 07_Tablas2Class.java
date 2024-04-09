fd503bd69759178b2502716236676d81f8facd4d
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-09 17:55:37 -0600

    Create 07_Tablas2Class.java

---
+++ 07_Tablas2Class.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        int resultado = 0;
+
+        Scanner Teclado = new Scanner (System.in);
+        System.out.println("Escribe un numero para sacar su tabla de multiplicar");
+        int numero = Teclado.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            resultado= numero * i;
+            System.out.println(resultado);
+        }
+    }
+}
