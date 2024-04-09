69d666c0ec5a42e3dec3731c9317146de435cd26
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-09 17:42:38 -0600

    Create 02_EdadContClass.java

---
+++ 02_EdadContClass.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        int numero = 1;
+
+        Scanner Teclado = new Scanner (System.in);
+        System.out.println("Escribe tu Edad");
+        int Edad = Teclado.nextInt();
+
+        while(numero <= Edad){
+            System.out.println(numero);
+            numero = numero + 1;
+        }
+    }
+}
