c1a46d9d7c894273798b0b928a24d50869b04f66
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-09 18:07:31 -0600

    Create 08_Piramide2Class.java

---
+++ 08_Piramide2Class.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        int contador = 1;
+        int contadorinicial = 1;
+        int contadorfinal = 2;
+        int numero2 = 1;
+        int aumentador = 1;
+
+        Scanner Teclado = new Scanner (System.in);
+        System.out.println("Escribe un numero entero positivo");
+        int numero = Teclado.nextInt();
+
+        while(contador <= numero){
+            while(contadorinicial < contadorfinal){
+                System.out.print(numero2 + " , ");
+                contadorinicial = contadorinicial + 1;
+                numero2 = numero2 - 2;
+            }
+            numero2 = 1;
+            aumentador = aumentador + 2;
+            numero2 = numero2 * aumentador;
+            contadorinicial = 1;
+            contadorfinal = contadorfinal + 1;
+
+            System.out.println();
+            contador = contador + 1;
+        }
+    }
+}
