291289218a46308aa788e36a9f37d1179d9d0fe1
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-09 18:31:09 -0600

    Create 09_PasswordClass.java

---
+++ 09_PasswordClass.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        Scanner Teclado = new Scanner (System.in);
+        System.out.println("Escribe una contraseña");
+        String pass1 = Teclado.next();
+
+        Scanner Teclado2 = new Scanner (System.in);
+        System.out.println("Escribe nuevamente la contraseña");
+        String pass2 = Teclado2.next();
+
+        if(pass1.equals(pass2)){
+            System.out.println("La contraseña es correcta");
+        }else{
+            System.out.println("La contraseña no es correcta");
+        }
+    }
+}
