f336fe840e86ad749852d61deac4619094d46dc3
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-11 09:01:38 -0600

    Actualizar 10_Tablas3Class.java

--- 10_Tablas3Class.java
+++ 10_Tablas3Class.java
@@ -6,7 +6,6 @@ public class main {
         int resultado = 0;
 
         for(int j = 1; j <= 10; j++){
-
            for(int i = 1; i <= 10; i++){
               resultado= numero * i;
               System.out.println(resultado);
