b0687222212169c22f18c0625876ecd4c05e2edc
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-11 09:01:20 -0600

    Actualizar

---
+++ 10_Tablas3Class.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        int resultado = 0;
+
+        for(int j = 1; j <= 10; j++){
+
+           for(int i = 1; i <= 10; i++){
+              resultado= numero * i;
+              System.out.println(resultado);
+           }
+           System.out.println();
+        }
+    }
+}
