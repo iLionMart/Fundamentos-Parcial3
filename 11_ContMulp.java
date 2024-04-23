9e9700a883c9abc49c22199fa610a8dec8a7b929
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-23 07:32:53 -0600

    Create 11_ContMulp.java

---
+++ 11_ContMulp.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.println("Introduce el numero limite para calcular");
+        Scanner numlimes = new Scanner(System.in);
+        int numlit = numlimes.nextInt();
+
+        int numini = 0;
+        int numcont = 3;
+        int resultado = 0;
+
+        while(numcont <= numlit){
+            System.out.println(numcont);
+            numcont = numcont + 3;
+            resultado = (resultado + numcont) - 3;
+            numini = numini + 1;
+        }
+
+        System.out.println("El resultado de la suma del multiplo de 3 es: " + resultado);
+        System.out.println("La cantidad de numeros es: " + numini);
+    }
+}
