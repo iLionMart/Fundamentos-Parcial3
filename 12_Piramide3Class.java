4a696ffebdeea02422d189a960b0884f792d9425
Lion Mart <45979134+LeonGamerGL@users.noreply.github.com>
2024-04-23 07:33:49 -0600

    Create 12_Piramide3Class.java

---
+++ 12_Piramide3Class.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        System.out.print("Introduzca un caracter: ");
+        Scanner caracterwa = new Scanner(System.in);
+        String caracter = caracterwa.next();
+
+        System.out.println();
+
+        System.out.print("Introduzca una altura: ");
+        Scanner alturawa = new Scanner(System.in);
+        int altura = alturawa.nextInt();
+
+        int cantidad = 1;
+        int espacios = 0;
+
+        for (int i = 1; i <= altura; i++){
+            if (i != altura){
+                for (int n = 0; n < espacios; n++){
+                    System.out.print(" ");
+                }
+                espacios --;
+                for (int n = 1; n <= cantidad; n++){
+                    if ((n == 1) || (n == cantidad)){
+                        System.out.print(caracter);
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+                cantidad += 2;
+                System.out.println();
+            }else{
+                for (int n = 0; n < espacios; n++){
+                    System.out.print(" ");
+                }
+                espacios --;
+                for (int n = 1; n <= cantidad; n++){
+                    System.out.print(caracter);
+                }
+            }
+        }
+    }
+}
