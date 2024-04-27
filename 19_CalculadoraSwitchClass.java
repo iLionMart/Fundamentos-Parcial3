55a3bd40e94fbeaefd7de0bf9d7e369d4e351643
Lion Mart <45979134+iLionMart@users.noreply.github.com>
2024-04-26 19:05:48 -0600

    Create 19_CalculadoraSwitchClass.java

---
+++ 19_CalculadoraSwitchClass.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main (String[] args) {
+
+        int resultado = 0;
+        String operacion = null;
+        
+        System.out.println("Ingresa el primer numero");
+        Scanner num1s = new Scanner (System.in);
+        int num1 = num1s.nextInt();
+
+        System.out.println("Ingresa el segundo numero");
+        Scanner num2s = new Scanner (System.in);
+        int num2 = num2s.nextInt();
+
+        System.out.println("Que tipo de operacion se va a realizar (suma/resta/multiplicacion/division)");
+        Scanner ope = new Scanner (System.in);
+        operacion = ope.next();
+
+        switch (operacion) {
+            case "suma":
+                resultado = num1 + num2;
+            break;
+            case "resta":
+                resultado = num1 - num2;
+            break;
+            case "multiplicacion":
+                resultado = num1 * num2;
+            break;
+            case "division":
+                resultado = num1 / num2;
+            break;
+        }
+
+        System.out.println("El resultado de tu " + operacion + " es " + resultado);
+    }    
+}
