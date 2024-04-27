1394dda5bbe19998138967aa842f3d309d136f6b
Lion Mart <45979134+iLionMart@users.noreply.github.com>
2024-04-26 18:49:27 -0600

    Create 18_SemanaSwitchClass.java

---
+++ 18_SemanaSwitchClass.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main (String[] args) {
+
+        String nomdia = null;
+        int dia = 0;
+
+        System.out.println("Introduce un numero para determinar el dia");
+        Scanner dian = new Scanner (System.in);
+        dia = dian.nextInt();
+        
+        switch (dia) {
+            case 1:
+                nomdia = "Lunes";
+            break;
+            case 2:
+                nomdia = "Martes";
+            break;
+            case 3:
+                nomdia = "Miercoles";
+            break;
+            case 4:
+                nomdia = "Jueves";
+            break;
+            case 5:
+                nomdia = "Viernes";
+            break;
+            case 6:
+                nomdia = "Sabado";
+            break;
+            case 7:
+                nomdia = "Domingo";
+            break;
+        }
+
+        System.out.println("El dia es: " + nomdia );
+    }
+}
