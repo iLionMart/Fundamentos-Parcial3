4fbd3cd19fe2c021484cff4f7805c4a16b9ddd06
Lion Mart <45979134+iLionMart@users.noreply.github.com>
2024-04-30 07:26:28 -0600

    Create 22_NumerosPrimosClass.java

---
+++ 22_NumerosPrimosClass.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class main {
+    public static void main (String[] args) {
+
+        int n = 0;
+        int x = 2;
+
+        String PrimoEntrada = JOptionPane.showInputDialog("Introduzca un número y se definira si es primo");
+        n = Integer.valueOf(PrimoEntrada);
+
+        if (n % x == 0) {
+            System.out.println("El numero " + n + " no es primo");
+        } else {
+            while (n%x != 0) {
+                x = x + 1;
+                if (n == x) {
+                    System.out.println("El numero " + n + " es primo");
+                }else if (n%x == 0){
+                    System.out.println("El numero " + n + " no es primo");
+                }
+            }
+        }
+    }
+}
