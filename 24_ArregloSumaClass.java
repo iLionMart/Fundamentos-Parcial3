7cd5e574f4909de0a742bccf7df732e184df9e55
Lion Mart <45979134+iLionMart@users.noreply.github.com>
2024-05-02 08:24:33 -0600

    Create 24_ArregloSumaClass.java

---
+++ 24_ArregloSumaClass.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+
+        int resultado = 0;
+        int[] num = new int[6];
+        num[1] = 3;
+        num[2] = 5;
+        num[3] = 2;
+        num[4] = 8;
+        num[5] = 6;
+
+        for (int i=1; i < 6; i++){
+            resultado += num[i];
+        }
+
+        System.out.println("La suma de los elementos es: " + resultado);
+    }
+}
