23b495da7d291b18451b04594857bbb5eed2f0d8
Lion Mart <45979134+iLionMart@users.noreply.github.com>
2024-05-02 08:24:58 -0600

    Create 25_ArregloPromedioClass.java

---
+++ 25_ArregloPromedioClass.java
@@ -0,0 +1,18 @@
+public class main {
+        public static void main(String[] args) {
+
+            int resultado = 0;
+            int[] cal = new int[5];
+            cal[1] = 8;
+            cal[2] = 6;
+            cal[3] = 5;
+            cal[4] = 9;
+
+            for (int i=1; i < 5; i++){
+                resultado += cal[i];
+            }
+            resultado = resultado/4;
+
+            System.out.println("El promedio del alumno es: " + resultado);
+        }
+    }
