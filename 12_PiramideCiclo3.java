7c896abc702221200445f96824a6334fb3cf9517
lildud <45979134+imlildud@users.noreply.github.com>
2025-02-18 21:54:19 -0600

    Update and rename 12_Piramide3Class.java to 12_PiramideCiclo3.java

--- 12_PiramideCiclo3.java
+++ 12_PiramideCiclo3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class PiramideCiclo3 {
     public static void main(String[] args) {
 
         System.out.print("Introduzca un caracter: ");
