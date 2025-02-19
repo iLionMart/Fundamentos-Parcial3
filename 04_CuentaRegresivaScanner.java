23e14a47b3b334b4bcd33dda00b5213d31a1195c
lildud <45979134+imlildud@users.noreply.github.com>
2025-02-18 21:42:40 -0600

    Update and rename 04_CuentaReg2Class.java to 04_CuentaRegresivaScanner.java

--- 04_CuentaRegresivaScanner.java
+++ 04_CuentaRegresivaScanner.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class CuentaRegresivaScanner {
     public static void main(String[] args) {
 
         Scanner Teclado = new Scanner (System.in);
